3b211854772b5bc0f03534b93121661867618f14
J_Mat <zhujieming2003@aliyun.com>
2022-02-27 22:01:42 +0800

    Init Engine

--- ZeroRealTimeSoftRenderer/src/main.cpp
+++ ZeroRealTimeSoftRenderer/src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include "win_platform.h"
 
+class ZeroRealTimeSoftEngine : public App
+{
+	ZeroRealTimeSoftEngine() : App() 
+	{
+	}
 
-int main()
+	virtual void Init()
+	{
+	}
+	
+	virtual void Render()
+	{
+	}
+	
+	virtual void ShutDown()
+	{
+
+	}
+};
+
+int main(int argc, const char** argv)
 {
 	std::cout << "Hello Zero Real Time Soft Engine!";
+	
+	ZeroRealTimeSoftEngine* app = new ZeroRealTimeSoftEngine();
+
+	app->Run(app);
+
+	delete app;
+	
 	return 0;
 }
 
