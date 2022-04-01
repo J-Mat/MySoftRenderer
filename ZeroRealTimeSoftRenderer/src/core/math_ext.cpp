064b826eb242248619c38aa144bd9b0674011812
J_Mat <zhujieming2003@aliyun.com>
2022-04-01 22:26:37 +0800

    裁剪重来

--- ZeroRealTimeSoftRenderer/src/core/math_ext.cpp
+++ ZeroRealTimeSoftRenderer/src/core/math_ext.cpp
@@ -1,8 +1,8 @@
-#pragma once
+﻿#pragma once
 #include "math_ext.h"
 
 /*
-* ��һ��math���м�㣬��ߣͶӰ����任���ﵽѧϰ��Ŀ�ģ���ʵglm��ɶ���У�
+* ×öÒ»¸ömathµÄÖÐ¼ä²ã£¬ÊÖß£Í¶Ó°¾ØÕó±ä»»£¬´ïµ½Ñ§Ï°µÄÄ¿µÄ£¨ÆäÊµglmÀïÉ¶¶¼ÓÐ£©
 */
 namespace Math
 {
@@ -13,20 +13,20 @@ namespace Math
 		vec3 forward = glm::normalize(eye - target);
 		vec3 left = glm::normalize(glm::cross(up, forward));
 		up = glm::normalize(glm::cross(forward, left));
-		
+
 		m[0][0] = left[0];
 		m[0][1] = left[1];
 		m[0][2] = left[2];
-		
+
 		m[1][0] = up[0];
 		m[1][1] = up[1];
 		m[1][2] = up[2];
-		
+
 		m[2][0] = forward[0];
 		m[2][1] = forward[1];
 		m[2][2] = forward[2];
 
-		m[3][0] = -glm::dot(left,eye);
+		m[3][0] = -glm::dot(left, eye);
 		m[3][1] = -glm::dot(up, eye);
 		m[3][2] = -glm::dot(forward, eye);
 
@@ -56,71 +56,53 @@ namespace Math
 		m[0][0] = 2 / x_range;
 		m[1][1] = 2 / y_range;
 		m[2][2] = 2 / z_range;
-		
+
 		m[0][3] = -(left + right) / x_range;
 		m[1][3] = -(bottom + top) / y_range;
 		m[2][3] = -(near + far) / z_range;
-		
+
 		return m;
 	}
 
-/*
- * fovy: the field of view angle in the y direction, in degrees
- * aspect: the aspect ratio, defined as width divided by height
- * near, far: the coordinates for the near and far clipping planes
- *
- * 1/(aspect*tan(fovy/2))              0             0           0
- *                      0  1/tan(fovy/2)             0           0
- *                      0              0  -(f+n)/(f-n)  -2fn/(f-n)
- *                      0              0            -1           0
- *
- * this is the same as
- *     float half_h = near * (float)tan(fovy / 2);
- *     float half_w = half_h * aspect;
- *     mat4_frustum(-half_w, half_w, -half_h, half_h, near, far);
- *
- * see http://www.songho.ca/opengl/gl_projectionmatrix.html
- *
- * note: my implementation is based on right-handed system, so it is a little different
- */
+	// https://zhuanlan.zhihu.com/p/144329075 
 	mat4 GetPerspectMat(float fovy, float aspect, float near, float far)
 	{
 		mat4 m = mat4(1.0);
 		fovy = glm::radians(fovy);
 		float t = std::abs(near) * std::tan(fovy / 2);
 		float r = aspect * t;
-		
+
 		m[0][0] = near / r;
 		m[1][1] = near / t;
 		m[2][2] = (near + far) / (near - far);
-		m[3][2] = -2 * near * far / (far - near);
-		m[2][3] = -1;
+		m[3][2] = near * far / (far - near);
+		m[2][3] = 1;
 		m[3][3] = 0;
-		
+
 		return m;
 	}
-	
-	
-	void GetTBN(vec3& T, vec3& B, vec3& N, vec3 & normal, vec2* texcoords, vec3* positions)
+
+
+	void GetTBN(vec3& T, vec3& B, vec3& N, vec3& normal, vec2* texcoords, vec3* positions)
 	{
 		float x1 = texcoords[1][0] - texcoords[0][0];
 		float y1 = texcoords[1][1] - texcoords[0][1];
-		
+
 		float x2 = texcoords[2][0] - texcoords[0][0];
 		float y2 = texcoords[2][1] - texcoords[0][1];
-		
+
 		float det = 1.0f / (x1 * y2 - x2 * y1);
-		
+
 		vec3 e1 = positions[1] - positions[0];
 		vec3 e2 = positions[2] - positions[0];
-		
+
 		vec3 t = e1 * y2 - e2 * y1;
 		vec3 b = e1 * (-x2) + e2 * x1;
-		
+
 		t *= det;
 		b *= det;
-		
-		// ˹��������
+
+		// Ë¹ÃÜÌØÕý½»
 		//https://www.matongxue.com/madocs/789/
 		N = normalize(normal);
 		T = normalize(t - dot(t, normal) * normal);
